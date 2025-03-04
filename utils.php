e477b6f50371f6348fe83001bfa87a9749081ed4
fasilofficial <muhammedfasilofficial@gmail.com>
2025-03-04 17:51:45 +0530

    Update property link

--- utils.php
+++ utils.php
@@ -81,11 +81,9 @@ function getUserId(array $filter): ?int
 function getPropertyLink($title)
 {
     $url = "https://seocrm.gicrm.ae/property-detail/";
-    
     $title = strtolower($title);
-    $title = str_replace(" | ", "-", $title);
-    $title = str_replace(" ", "-", $title);
+    $title = str_replace([" | ", " "], "-", $title);
     $title = preg_replace('/[^a-z0-9\-]/', '', $title);
 
-    return urlencode($url . $title);
+    return $url . $title;
 }
